0cdefd424502326841e4ec1af3158d884ffcad4b
Lakhveer Bawa <bawa93lakhveer@gmail.com>
2020-12-29 14:16:41 +0530

    Testing Working Okay

--- test/Sample.test.js
+++ test/Sample.test.js
@@ -1,30 +1,33 @@
-const { resolve } = require('path')
-const { Nuxt, Builder } = require('nuxt')
-// const request = require('supertest')
 
+const { Nuxt, Builder } = require('nuxt')
+// eslint-disable-next-line no-unused-vars
+const request = require('supertest')
+const nuxtConfig = require('../nuxt.config.js')
 // We keep the nuxt and server instance
 // So we can close them at the end of the test
 let nuxt = null
 
 // Init Nuxt.js and create a server listening on localhost:4000
 beforeAll(async () => {
-  const config = {
-    dev: process.env.NODE_ENV === 'production',
-    rootDir: resolve(__dirname, '../'),
-    mode: 'universal',
-  }
+  // const config = {
+  //   dev: process.env.NODE_ENV === 'production',
+  //   rootDir: resolve(__dirname, '../'),
+  //   mode: 'universal',
+  //   plugins,
+  //   modules
+  // }
 
-  nuxt = new Nuxt(config)
+  nuxt = new Nuxt({...nuxtConfig, server: { port: 3001}, buildDir: '.nuxt-build-jest'})
 
   await new Builder(nuxt).build()
 
   await nuxt.server.listen(3001, 'localhost')
-}, 30000)
+}, 300000)
 
 // Example of testing only generated html
 describe('GET /', () => {
   test('Route / exits and render HTML', async () => {
-    const { html } = await nuxt.renderRoute('/', {})
+    const { html } = await nuxt.server.renderRoute('/', {})
 
     expect(html).toContain('Vuetify')
   })
